50c600b9e2cb6babc5417d8c4cb86f413bc60df6
Azuma-9 <azuma1907@gmail.com>
2026-08-18 21:26:52 +0900

    Inital commit

---
+++ src/utils/time.js
@@ -0,0 +1,14 @@
+export function formatTime(hour, minute) {
+  const period = hour < 12 ? '午前' : '午後';
+  let h = hour % 12;
+  if (h == 0) h = 12;
+  const mm = String(minute).padStart(2, '0');
+  return `${period}${h}:${mm}`;
+}
+
+export const timeOptions = [];
+for (let h = 9; h < 19; h++) {
+  for (let m = 0; m < 60; m += 30) {
+    timeOptions.push(formatTime(h, m));
+  }
+}
